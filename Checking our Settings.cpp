25b47957604ea1fb36b45699099fb60b97363d0c
anuforfun26-glitch <anuforfun26@gmail.com>
2026-08-05 18:56:57 +0530

    Add files via upload

---
+++ Checking our Settings.cpp
@@ -0,0 +1,16 @@
+// Hello! I am restarting after a hot minute so just spend an hour revising Chp 0 
+// the many preconfigurations that need to be done. So just testing them.
+
+void foo(int)
+{
+}
+
+int main()
+{
+	unsigned int x{ 5 };
+	foo(x);
+
+	return 0; // technically since main is an implicit function no need to write it 
+	          // but its good practice :)
+
+}
