d777f3ea8f10bd9d306cb24db94556c50519c929
anuforfun26-glitch <anuforfun26@gmail.com>
2026-08-08 15:37:26 +0530

    Add files via upload

--- LearnCPP_1.5_BufferTopic_Part3.cpp
+++ LearnCPP_1.5_BufferTopic_Part3.cpp
@@ -6,7 +6,7 @@ int main()
 	int x{};
 	std::cin >> x;
 
-	int y{};
+	long long y{};
 	std::cin >> y;
 
 	std::cout << "you entered" << x << "and" << y << "\n";
@@ -17,3 +17,6 @@ int main()
 // First time we did traditionally, Put a value for x , click enter then put a value of y then got the result
 // However, if we had , in the first go put 4 - space- 5,then click enter, it takes both at once and gives same result as above
 // this is bcuz in 2nd run, we gave the buffer 2 values in first go! it took the first value (4) then paused at space bar, when the second cin needed a value it SIMPLY TOOK WHATEVER WAS LEFT (or trapped) IN BUFFER WITHOUT ASKING U!
+//Another cool fact. If u enter a number larger than 2147483647, then it will simply print 214.., this is bcuz in standard int is alloctaed 4 bytes, that is 32 bits, now WE have only 31 bcuz one bit is needed just to track the sign
+// so maximum binary combinations for 31 bits is , 214...
+// to put a larger number, u must change data type to long long- which upgrades to 8 byte or 64 bit container
